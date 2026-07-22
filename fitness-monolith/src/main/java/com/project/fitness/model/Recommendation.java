a4fb5e61420e84b2a6f3c0134f462f9022493d66
atharvaparkale <atharva.parkale@gmail.com>
2026-07-22 17:18:34 +0530

    :memo: add relationship types

--- fitness-monolith/src/main/java/com/project/fitness/model/Recommendation.java
+++ fitness-monolith/src/main/java/com/project/fitness/model/Recommendation.java
@@ -15,12 +15,12 @@ public class Recommendation {
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(name = "fk_activity_user"))
     @JsonIgnore
     private User user;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "activity_id", nullable = false, foreignKey = @ForeignKey(name = "fk_activity_user"))
     @JsonIgnore
     private Activity activity;

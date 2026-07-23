7b0811b284c15e5476a88c41d8202c5c0e3b5e0b
atharvaparkale <atharva.parkale@gmail.com>
2026-07-24 01:47:48 +0530

    :memo: add get activity changes

--- fitness-monolith/src/main/java/com/project/fitness/controller/ActivityController.java
+++ fitness-monolith/src/main/java/com/project/fitness/controller/ActivityController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/activities")
 @RequiredArgsConstructor
@@ -16,5 +18,10 @@ public class ActivityController {
     @PostMapping
     public ResponseEntity<ActivityResponse> trackActivity(@RequestBody ActivityRequest request) {
         return ResponseEntity.ok(activityService.trackActivity(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ActivityResponse>> getUserActivities(@RequestHeader(value = "X-User-ID") String userId) {
+        return ResponseEntity.ok(activityService.getUserActivities(userId));
     }
 }
--- fitness-monolith/src/main/java/com/project/fitness/service/ActivityService.java
+++ fitness-monolith/src/main/java/com/project/fitness/service/ActivityService.java
@@ -9,6 +9,9 @@ import com.project.fitness.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class ActivityService {
@@ -39,5 +42,10 @@ public class ActivityService {
         response.setUpdatedAt(savedActivity.getUpdatedAt());
 
         return response;
+    }
+
+    public List<ActivityResponse> getUserActivities(String userId) {
+        List<Activity> activityList = activityRepository.findByUserId(userId);
+        return activityList.stream().map(this::mapToResponse).collect(Collectors.toList());
     }
 }

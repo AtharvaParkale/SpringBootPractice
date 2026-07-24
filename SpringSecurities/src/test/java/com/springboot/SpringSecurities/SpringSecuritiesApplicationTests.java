be44b22d797ffd9136ecd47afb0979b3ef579c37
atharvaparkale <atharva.parkale@gmail.com>
2026-07-24 14:27:19 +0530

    :memo: setup spring securities project

---
+++ SpringSecurities/src/test/java/com/springboot/SpringSecurities/SpringSecuritiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.SpringSecurities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

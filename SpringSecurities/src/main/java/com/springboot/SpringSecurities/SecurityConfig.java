5c94f6faf14b37be0ca76b750d163c5fc21605e6
atharvaparkale <atharva.parkale@gmail.com>
2026-07-24 17:24:18 +0530

    :memo: add multiple basic auth impl

--- SpringSecurities/src/main/java/com/springboot/SpringSecurities/SecurityConfig.java
+++ SpringSecurities/src/main/java/com/springboot/SpringSecurities/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,12 +17,19 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
-
         http.authorizeHttpRequests(authorizeRequests ->
                 authorizeRequests.anyRequest().authenticated());
-
         http.httpBasic(Customizer.withDefaults());
-
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withUsername("user1").password("{noop}password1").build();
+        UserDetails user2 = User.withUsername("user2").password("{noop}password2").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}admin").build();
+
+
+        return new InMemoryUserDetailsManager(user1,user2, admin);
     }
 }

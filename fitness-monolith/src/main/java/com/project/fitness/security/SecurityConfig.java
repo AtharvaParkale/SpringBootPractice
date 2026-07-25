a648fdfbf3c4b408964a9e376279fe462d32b4c4
atharvaparkale <atharva.parkale@gmail.com>
2026-07-26 02:57:39 +0530

    :memo: add swagger documentation

--- fitness-monolith/src/main/java/com/project/fitness/security/SecurityConfig.java
+++ fitness-monolith/src/main/java/com/project/fitness/security/SecurityConfig.java
@@ -26,7 +26,9 @@ public class SecurityConfig {
                                 .requestMatchers("/api/admin/**")
                                 .hasRole("ADMIN")
                                 .requestMatchers("/api/auth/**").permitAll()
-                                .anyRequest().authenticated());
+                                .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
+                                .anyRequest()
+                                .authenticated());
 
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
